2315a8d9e9459375b6d34617837097c915f76b56
NaumenkoAndrei <98833014+NaumenkoAndrei@users.noreply.github.com>
2024-02-20 19:35:14 +0400

    Полная реализация логики Гриба
    Реализован код и проведён рефакторинг

--- Assets/Scripts/Monsters/Mushroop.cs
+++ Assets/Scripts/Monsters/Mushroop.cs
@@ -1,86 +1,37 @@
 using UnityEngine;
+using System.Collections;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Mushroop : Enemies
 {
-    [SerializeField] private Transform target; 
-    [SerializeField] private float moveSpeed = 3f; 
-    [SerializeField] private float attackRange = 1f; 
-    [SerializeField] private float viewDistance = 15f;
-    private SpriteRenderer sprite;
-
-    private Rigidbody2D rb;
-    private bool isAttacking = false;
-
-
-    Vector3 lastKnownPlayerPosition;
-
-    void Start()
+    private void Start()
     {
-        lives = 3;
+        attackRange = 2.2f;
+        attackCooldown = 1.5f;
+        lives = 6;
+
+        StartCoroutine(BehaviorPattern());
     }
 
-    private void Awake()
+    private IEnumerator BehaviorPattern()
     {
-        rb = GetComponent<Rigidbody2D>();
-        sprite = GetComponentInChildren<SpriteRenderer>();
-    }
-
-    public override void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject == Hero.Instance.gameObject)
+        while (true)
         {
-            target = collision.gameObject.transform;
-            isDamaging = true;
-            ApplyDamage();
+            yield return null;
+
+            if (getDamage)
+            {
+                lastAttackTime = Time.time;
+                continue;
+            }
+
+            if (CanAttackPlayer() && Time.time > lastAttackTime + attackCooldown)
+                yield return StartCoroutine(ExecuteAttack());
+            else
+                State = States.idle;
+
+            sprite.flipX = player.position.x < transform.position.x;
         }
-        else
-            rb.velocity = Vector2.zero;
-    }
-
-
-    private void FixedUpdate()
-    {
-        CheckPlayerPosition();
-    }
-    
-    void CheckPlayerPosition()
-    {
-        Vector3 targetDirection = target.position - transform.position;
-
-        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, targetDirection.normalized, targetDirection.magnitude);
-        foreach (RaycastHit2D hit in hits)
-            if (!hit.collider.CompareTag("Player"))
-                return;
-
-        RaycastHit2D playerHit = Physics2D.Raycast(transform.position, targetDirection.normalized, Mathf.Infinity);
-        if (playerHit.collider != null && playerHit.collider.CompareTag(tag: "Player"))
-        {
-            lastKnownPlayerPosition = playerHit.collider.transform.position;
-
-            float distanceToTarget = Vector3.Distance(transform.position, target.position);
-
-            if (distanceToTarget <= attackRange && !isAttacking)
-                Attack();
-            else if (distanceToTarget <= viewDistance)
-                MoveTowardsTarget(target.position);
-        }
-        else
-            MoveTowardsTarget(lastKnownPlayerPosition);
-    }
-
-    void MoveTowardsTarget(Vector3 targetPosition)
-    {
-        Vector3 direction = (target.position - transform.position).normalized;
-
-        rb.velocity = new Vector2(direction.x * moveSpeed, rb.velocity.y);
-        sprite.flipX = direction.x < 0.0f;
-    }
-
-    void Attack()
-    {
-        isAttacking = true;
-        // Здесь можно добавить задержку между атаками
-        // Например, через Coroutine или использование таймера
     }
 }
 
